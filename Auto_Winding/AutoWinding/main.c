9329da2ce6014b90525a407e99868adcf92fbd60
BadEnginer <badenginer@gmail.com>
2017-09-01 16:42:57 +1000

    Добавил работу с LCD и .. все

--- Auto_Winding/AutoWinding/main.c
+++ Auto_Winding/AutoWinding/main.c
@@ -1,9 +1,11 @@
 #define F_CPU 16000000L
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include "util/delay.h"
 #include "as5048/as5048.h"
 #include "timers/timer_vi.h"
 #include "viS/Vi_S.h"
+#include "LCD/lcd_lib_2.h"
 uint16_t time = 0;
 TaskQueue qTasks;
 Task	tIplus;
@@ -12,41 +14,36 @@ Task	tYplus;
 
 ISR(TIMER0_COMP_vect){
 	time++;
-	 ServerTime(&qTasks);
-}
-
-void Iplus(){
-	static uint16_t Ip=0;
-	Ip++;
-	RunTask(&qTasks, &tUplus, CONTINUOUS, 5);
-	RunTask(&qTasks, &tYplus, CONTINUOUS, 100);
-}
-void Uplus(){
-	static uint8_t i = 0;
-	static uint16_t Up[10]={0};
-	Up[i] = time;
-	i++;
-	if(i>9)
-		i = 0;
-}
-void Yplus(){
-	static uint16_t Yp=0;
-	Yp = (i2cRead(AS5048_ADR,AS5048_ANGLE_HI) << 6);
+	 //ServerTime(&qTasks);
 }
 
 int main(){
 	initI2C();
+	LCD_Init();
+	LCD_Goto(0,0);
+	uint16_t i = getPosition();
+	char c[10] = {0};
+	
 	initTime0(DIV_64,CTC,249,EN_OCF);
 	InitVI_OS(&qTasks); // Инициализирует весь деспетчер
-	InitTask(&tIplus, Iplus, 10);
-	InitTask(&tUplus, Uplus, 5);
-	InitTask(&tYplus, Yplus, 10); 
-	RunTask(&qTasks, &tIplus, DISPOSABLE, 10);
-
-	
+	RunTask(&qTasks, &tIplus, DISPOSABLE, 0);
 	sei();
 	while (1){
-		
-		TaskManager(&qTasks);
+		_delay_ms(100);
+		i = getPosition();
+		LCD_Goto(0,0);
+		c[0] = (i/1000) + '0';
+			i %=1000;
+		c[1] = (i/100) + '0';
+			i %=100;
+		c[2] = (i/10)+'0';
+			i %=10;
+		c[3] = i+'0';
+		LCD_WriteData(c[0]);
+		LCD_WriteData(c[1]);
+		LCD_WriteData(c[2]);
+		LCD_WriteData('.');
+		LCD_WriteData(c[3]);
+		//TaskManager(&qTasks);
 	}
 }
